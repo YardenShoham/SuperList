16b1190fecaaca09e04a34a22f933c30f7ef37fb
YardenShoham <hrsi88@gmail.com>
2018-09-28 17:01:56 +0300

    Added SuperList class

---
+++ Src/SuperList.java
@@ -0,0 +1,45 @@
+// SuperList.java
+
+import java.util.*;
+
+import java.io.*;
+
+public class SuperList
+{
+	private ArrayList<Product> theList;
+	private String location = "theList.bin";
+
+	public SuperList()
+	{
+		try
+		{
+			File theListFile = new File(location);
+			if (theListFile.exists() && !theListFile.isDirectory())
+			{
+				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(location));
+				theList = (ArrayList<Product>) ois.readObject();
+				ois.close();
+			}
+			else
+			{
+				theList = new ArrayList<>();
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+			theList = new ArrayList<>();
+		}
+	}
+
+	public void addToList(Product newProduct) 
+	{
+		theList.add(newProduct);
+	}
+
+	public boolean removeFromList(Product product)
+	{
+		return theList.remove(product);
+	}
+
+}
